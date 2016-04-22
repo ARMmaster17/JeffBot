3214508b267464ddfc3316aa8374a544901c0bf0
Joshua Zenn <jzenn@students.kennesaw.edu>
2016-04-22 21:50:48 +0000

    Fixed infinite message loop

--- frontend/main.rb
+++ frontend/main.rb
@@ -28,16 +28,14 @@ end
 post '/api/v1/extern/groupme' do
     request.body.rewind
     data = request.body.read
-    puts data
     inbound_payload = JSON.parse(data)
     inbound_message = inbound_payload["text"]
     inbound_sender = inbound_payload["name"]
-    #if inbound_message.split(" ")[0].eql?(inbound_payload["@" + ENV["GROUPME_BOT_NAME"]])
-    resp = Hash.new
-    resp["bot_id"] = ENV["GROUPME_BOT_ID"]
-    resp["text"] = "@" + inbound_sender + ": " + Botsolver.go(inbound_message.sub("@" + ENV["GROUPME_BOT_NAME"] + " ", ""))
-    outbound_payload = resp.to_json
-    puts resp
-    result = RestClient.post('https://api.groupme.com/v3/bots/post', outbound_payload, :content_type => :json)
-    #end
+    if inbound_message.split(" ")[0].eql?(inbound_payload["@" + ENV["GROUPME_BOT_NAME"]])
+        resp = Hash.new
+        resp["bot_id"] = ENV["GROUPME_BOT_ID"]
+        resp["text"] = "@" + inbound_sender + ": " + Botsolver.go(inbound_message.sub("@" + ENV["GROUPME_BOT_NAME"] + " ", ""))
+        outbound_payload = resp.to_json
+        result = RestClient.post('https://api.groupme.com/v3/bots/post', outbound_payload, :content_type => :json)
+    end
 end
