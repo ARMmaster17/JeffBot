9befa26f99a711159765970a2ae1d4477880e174
Joshua Zenn <jzenn@students.kennesaw.edu>
2016-04-22 20:57:04 +0000

    Added GroupMe support

--- frontend/main.rb
+++ frontend/main.rb
@@ -24,3 +24,18 @@ end
 post '/api/v1/post' do
     Botsolver.go(params[:arg])
 end
+post '/api/v1/extern/groupme' do
+    request.body.rewind
+    data = request.body.read
+    puts data
+    inbound_payload = JSON.parse(data)
+    inbound_message = inbound_payload['text']
+    inbound_sender = inbound_payload['name']
+    if inbound_message.split(" ")[0].eql?(inbound_payload['@' + ENV["GROUPME_BOT_NAME"]])
+        resp = Hash.new
+        resp['bot_id'] = ENV['GROUPME_BOT_ID']
+        resp['text'] = "@#{inbound_sender}: #{Botsolver.go(inbound_message)}"
+        outbound_payload = resp.to_json
+        result = RestClient.post('https://api.groupme.com/v3/bots/post', outbound_payload)
+    end
+end
