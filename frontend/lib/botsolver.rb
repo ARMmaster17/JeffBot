27336521c76048fabb6918ad223c3cbcafa655c0
Joshua Zenn <jzenn@students.kennesaw.edu>
2016-04-22 12:13:51 +0000

    Fixed 'Say' query bug

--- frontend/lib/botsolver.rb
+++ frontend/lib/botsolver.rb
@@ -5,7 +5,7 @@ module Botsolver
         query = Lexicon.clean(arg)
         if query.eql?("what is your name")
             return "Jeff"
-        elsif query.match(/say .+?/)
+        elsif query.match(/^say .+?/)
             return query.sub("say ", "")
         else
             return "My name Jeff"
