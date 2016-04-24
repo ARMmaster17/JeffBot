814b77df51dc35a2b1aef244573edde94db7e037
Joshua Zenn <jzenn@students.kennesaw.edu>
2016-04-24 01:32:27 +0000

    Testing code

--- backend/lib/markov.rb
+++ backend/lib/markov.rb
@@ -5,6 +5,8 @@ require_relative 'formulator'
 module Markov
     def go(query)
         grams = Ngram.bigram(query)
+        puts grams
+        return "TESTING MODE...DO NOT RESPOND"
         Learn.train_array(grams)
         subject = Lexicon.get_subject(query)
         return Formulator.create_sentence_random(subject)
