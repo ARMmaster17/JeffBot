d515033336908b3f089f740f9c77b5077556ca0e
Joshua Zenn <wordman05@gmail.com>
2016-04-26 13:59:03 -0400

    Attempted fix for #19

--- frontend/public/src/js/chathandler.js
+++ frontend/public/src/js/chathandler.js
@@ -2,7 +2,16 @@ $('#msgTrigger').on('click', function (e) {
     var msg = $('#inputArg').val();
     $('#inputArg').val("");
     $("#messageQueue").prepend("<div class=\"message_container\"><div class=\"message_user\">" + msg + "</div></div>");
-    $.post("api/v1/post", {arg: msg}, function(result){
-        $("#messageQueue").prepend("<div class=\"message_container\"><div class=\"message_jeff\">" + result + "</div></div>");
+    //$.post("api/v1/post", {arg: msg}, function(result){
+    //    $("#messageQueue").prepend("<div class=\"message_container\"><div class=\"message_jeff\">" + result + "</div></div>");
+    //});
+    $.ajax({
+        type: "POST",
+        url: "api/v1/post",
+        data: {arg: msg},
+        timeout: 20000
+        success: function(result, status, xhr){
+            $("#messageQueue").prepend("<div class=\"message_container\"><div class=\"message_jeff\">" + result + "</div></div>");
+        }
     });
 })
