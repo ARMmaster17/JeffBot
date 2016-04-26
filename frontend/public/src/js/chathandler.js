57eaf01cc1e505dc72e64b883b2f21356d0a51fc
Joshua Zenn <wordman05@gmail.com>
2016-04-26 14:10:58 -0400

    Update chathandler.js

--- frontend/public/src/js/chathandler.js
+++ frontend/public/src/js/chathandler.js
@@ -1,5 +1,7 @@
 $('#msgTrigger').on('click', function (e) {
     var msg = $('#inputArg').val();
+    var payload = new Array();
+    payload['arg'] = msg;
     $('#inputArg').val("");
     $("#messageQueue").prepend("<div class=\"message_container\"><div class=\"message_user\">" + msg + "</div></div>");
     //$.post("api/v1/post", {arg: msg}, function(result){
@@ -8,7 +10,7 @@ $('#msgTrigger').on('click', function (e) {
     $.ajax({
         type: "POST",
         url: "api/v1/post",
-        data: {arg: msg},
+        data: payload,
         timeout: 20000,
         success: function(result, status, xhr){
             $("#messageQueue").prepend("<div class=\"message_container\"><div class=\"message_jeff\">" + result + "</div></div>");
