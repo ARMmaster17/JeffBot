76c252f09ae8546f486f92f286d93f71221e6eac
Dean <deangalvin3@gmail.com>
2016-04-27 17:27:48 -0400

    fixed formulator to use multiword

--- app/lib/formulator.rb
+++ app/lib/formulator.rb
@@ -7,15 +7,20 @@ module Formulator
     result << subject.chomp
     previous_token = subject
     token = WordChain.bigram_word(previous_token)
-    for _ in 0..length
-      break if token.eql?("") || token.eql?(nil) || iterator > length
+    for _ in 0..length*5
+      break if token.blank?
       result << token.chomp
       earlier_token = previous_token
       previous_token = token
       token = WordChain.next_word([earlier_token, previous_token])
+      if token.blank? || ([false]*5+[true]).sample
+        break if [true, false, false].sample
+        token = WordChain.bigram_word(previous_token)
+      end
     end
 
-    result.join(' ')
+    result[0] = result[0].titleize
+    "#{result.join(' ')}."
   end
 
   def Formulator.create_sentence_random(subject)
