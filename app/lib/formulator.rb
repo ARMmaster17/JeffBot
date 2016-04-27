ee05fa9461f8d1275007000fc4bc0beacb4a65e9
Dean <deangalvin3@gmail.com>
2016-04-27 15:46:04 -0400

    fixed code style and single word answers

--- app/lib/formulator.rb
+++ app/lib/formulator.rb
@@ -1,27 +1,24 @@
 require_relative 'wordchain'
 
 module Formulator
-    def Formulator.create_sentence(length, subject)
-        result = ""
-        previous_token = ""
-        iterator = 0
-        while (length > iterator)
-            if iterator == 0
-                result = subject + " "
-                previous_token = subject
-            else
-                token = WordChain.next_word(previous_token)
-                if token.eql?("")
-                    break
-                end
-                previous_token = token
-                result = result + token + " "
-            end
-            iterator = iterator + 1
-        end
-        return result.strip
+  def Formulator.create_sentence(length, subject)
+    iterator = 0
+    result = []
+    result << subject.chomp
+    previous_token = subject
+    token = WordChain.bigram_word(previous_token)
+    for _ in 0..length
+      break if token.eql?("") || token.eql?(nil) || iterator > length
+      result << token.chomp
+      earlier_token = previous_token
+      previous_token = token
+      token = WordChain.next_word([earlier_token, previous_token])
     end
-    def Formulator.create_sentence_random(subject)
-        return Formulator.create_sentence(3 + rand(8), subject)
-    end
+
+    result.join(' ')
+  end
+
+  def Formulator.create_sentence_random(subject)
+    return Formulator.create_sentence(3 + rand(8), subject)
+  end
 end
